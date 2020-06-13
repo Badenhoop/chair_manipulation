76f6e1726479eebd29d58c64acfe38b0fd8004c4
Philipp Badenhoop <philipp.badenhoop@hellocodeblog.com>
2020-06-13 13:11:59 +0200

    Initial commit
    Implement basic pipeline to segment the chair pointcloud from the
    ground plane.

---
+++ cmake-build-debug/CMakeFiles/detection_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/detection_node.dir/src/detection.cpp.o"
+  "devel/lib/chair_grasp_detection/detection_node"
+  "devel/lib/chair_grasp_detection/detection_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/detection_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/philipp/chair_manipulation_ws/src/chair_grasp_detection
+# Build directory: /home/philipp/chair_manipulation_ws/src/chair_grasp_detection/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
