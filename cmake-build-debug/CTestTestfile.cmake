f12af9e480b6fad5e70b8f02bb69bb429804ce30
Philipp Badenhoop <philipp.badenhoop@hellocodeblog.com>
2020-06-14 16:42:00 +0200

    Implement basic iterative closest point registration

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/philipp/chair_manipulation_ws/src/chair_grasp_detection
-# Build directory: /home/philipp/chair_manipulation_ws/src/chair_grasp_detection/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
